c32ac3ea80af13981450909168c38eb3b7b6533a
Nahukre <79340552+Nahukre@users.noreply.github.com>
2021-06-26 13:37:31 -0300

    trabajo 3

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,3 +1,4 @@
+//CLASE 2
 // const solicitarNumero = parseInt(prompt("Introduzca un número"));
 // if (solicitarNumero > 1000) {
 //     alert("El número es superior a 1000")
@@ -21,7 +22,7 @@
 //     alert("Excelente! Sigue así.")
 // } else alert("La nota ingresada es incorrecta. Intente nuevamente.")
 
-
+CLASE 3
 let ingresarNumero = parseInt(prompt("Ingresar número"));
 for (let i = 1; i <= 100; i++) {
     let resultado = ingresarNumero + i;
@@ -34,22 +35,22 @@ while (entrada != "ESC") {
     entrada = prompt("ingresar otro dato");
 }
 
-let dato = prompt("Ingresar un número entre 1 y 5");
+let dato = parseInt(prompt("Ingresar un número entre 1 y 5"));
 while (dato != "ESC") {
     switch (dato) {
-        case "1":
+        case 1:
             alert("HOLA");
             break;
-        case "2":
+        case 2:
             alert("HOLA HOLA");
             break;
-        case "3":
+        case 3:
             alert("HOLA HOLA HOLA")
             break;
-        case "4":
+        case 4:
             alert("HOLA HOLA HOLA HOLA")
             break;
-        case "5":
+        case 5:
             alert("HOLA HOLA HOLA HOLA HOLA")
             break;
         default:
@@ -58,3 +59,29 @@ while (dato != "ESC") {
     }
     dato = prompt("Ingresar un número entre 1 y 5 por favor. O escriba ESC para continuar.");
 }
+
+//CLASE 4 trabajo complementario
+//calculo de iva e ingresos brutos
+// const suma = (a, b) => a + b;
+// const resta = (a, b) => a - b;
+// let precioProducto = parseInt(prompt("Cuanto sale su producto sin impuestos"));
+// const iva = x => x * 0.21;
+// const precioDescuento = precioProducto * 0.2;
+// let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), precioDescuento);
+// let ingresosBrutos = x => x * 0.03;
+// let precioFinal = suma(nuevoPrecio, ingresosBrutos(precioProducto));
+// console.log("Usted pagará de IVA $" + iva(precioProducto));
+// console.log("Usted tiene un descuento de $" + precioDescuento);
+// console.log("El monto a pagar por Ingresos Brutos es de $" + ingresosBrutos(precioProducto));
+// console.log("El precio final es $" + precioFinal);
+
+// //saber si es multiplo
+// const multiplos = (a, b) => a % b;
+// var numero = parseInt(prompt("ingrese número"));
+// var multiplo = parseInt(prompt("Ingrese un número para saber si es multiplo"));
+// let resultado = multiplos(numero, multiplo);
+// if (resultado == 0) {
+//     alert(numero + " es multiplo de " + multiplo)
+// } else {
+//     alert(numero + " no es multiplo de " + multiplo)
+// }
